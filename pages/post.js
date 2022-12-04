7b4a306e6d38a9d120bb53b65533dade2ee4524e
Praveen Dias <praveendias1180@gmail.com>
2022-12-04 20:38:19 +0530

    Protect Premium Content 💲

--- pages/post.js
+++ pages/post.js
@@ -1,15 +1,34 @@
 //pages/post.js
 import React from "react";
-import { Heading, Text, View } from "@aws-amplify/ui-react";
+import {Authenticator,Flex,Heading,Text,useTheme,View,} from "@aws-amplify/ui-react";
 import Layout from "../components/Layout";
-import { Footer, Stories } from "../src/ui-components";
+import { Footer, Logo, Stories } from "../src/ui-components";
 function Post() {
+
+    const authComponents = {
+        Header() {
+            const { tokens } = useTheme();
+            return (
+                <Flex
+                    justifyContent={"center"}
+                    direction="column"
+                    paddingTop={tokens.space.xxxl}
+                    paddingBottom={tokens.space.xxl}
+                >
+                    <Logo />
+                </Flex>
+            );
+        },
+    };
+
     return (
-        <Layout
-            handleClick={() => { }}
+        <Authenticator components={authComponents} hideSignUp={true}>
+            {({ signOut, user }) => (
+                <Layout
+                    handleClick={() => signOut()}
             authText="Sign Out"
-            username={"christian@hotmail.com".split("@")[0]}
-        >
+            username={user.attributes.email.split("@")[0]}
+          >
             <View marginTop="50px" marginBottom="30px">
                 <Heading level={2}>Welcome to our backstage</Heading>
                 <Text marginTop="20px">
@@ -20,7 +39,9 @@ function Post() {
             <View marginTop="50px" marginBottom="50px">
                 <Footer />
             </View>
-        </Layout>
+            </Layout>
+      )}
+    </Authenticator>
     );
 }
 export default Post;
